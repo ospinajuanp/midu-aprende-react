8f07fa0b6eab02534f537d204788a51ba6a0a2a2
Ospinajuanp <juanpo996@gmail.com>
2024-03-01 10:32:32 -0500

    vite project and firt component no style

---
+++ 00-hola-mundo/src/App.jsx
@@ -0,0 +1,17 @@
+export function App (){
+    return(
+        <article>
+            <header>
+                <img src="https://unavatar.io/midudev" alt="" />
+                <div>
+                    <strong>Miguel Angel Duran</strong>
+                    <span>@midudev</span>
+                </div>
+            </header>
+            <aside>
+                <button type="button">Seguir</button>
+            </aside>
+        </article>
+    )
+
+}
