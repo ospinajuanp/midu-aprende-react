2e352b2e3e6bbdf79614e8f46464113e2735cf32
Ospinajuanp <juanpo996@gmail.com>
2024-03-04 23:22:33 -0500

    extract logic fact js

--- 04-react-prueba-tecnica/src/App.jsx
+++ 04-react-prueba-tecnica/src/App.jsx
@@ -1,20 +1,13 @@
 import { useState, useEffect } from 'react'
+import { getRandomFact } from './services/facts'
 import './App.css'
-
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
-// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
 
 export function App () {
   const [fact, setFact] = useState()
   const [imageURL, setImageURL] = useState()
 
   useEffect(() => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then((response) => response.json())
-      .then((data) => {
-        const { fact } = data
-        setFact(fact)
-      })
+    handleClick()
   }, [])
 
   useEffect(() => {
@@ -25,9 +18,15 @@ export function App () {
     setImageURL(`https://cataas.com/cat/says/${firstWord}?size=50&color=red`)
   }, [fact])
 
+  const handleClick = async () => {
+    const newFact = await getRandomFact()
+    setFact(newFact)
+  }
+
   return (
     <main>
       <h1>Hello World!</h1>
+      <button onClick={handleClick}>Get new fact</button>
       {fact && <p>{fact}</p>}
       {imageURL && <img src={imageURL} alt='Cat' />}
     </main>
