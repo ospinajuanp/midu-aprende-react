fba202c7f51d3c9aacee9a9e6bcac7b119336f65
Ospinajuanp <juanpo996@gmail.com>
2024-03-04 22:55:24 -0500

    result basic prueba tecnica

--- 04-react-prueba-tecnica/main.jsx
+++ 04-react-prueba-tecnica/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import { App } from './src/App'
+import { App } from './src/App.jsx'
 
 const root = createRoot(document.getElementById('app'))
 root.render(<App />)
--- 04-react-prueba-tecnica/src/App.jsx
+++ 04-react-prueba-tecnica/src/App.jsx
@@ -1,5 +1,33 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+
 export function App () {
+  const [fact, setFact] = useState()
+  const [imageURL, setImageURL] = useState()
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then((response) => response.json())
+      .then((data) => {
+        const { fact } = data
+        setFact(fact)
+
+        // const firstWord = fact.split(' ')[0].slice(0, 3).join(' ')
+        const firstWord = fact.split(' ', 3).join(' ')
+        console.log(firstWord)
+
+        setImageURL(`https://cataas.com/cat/says/${firstWord}?size=50&color=red`)
+      })
+  }, [])
+
   return (
-    <h1>Hello World!</h1>
+    <main>
+      <h1>Hello World!</h1>
+      {fact && <p>{fact}</p>}
+      {imageURL && <img src={imageURL} alt='Cat' />}
+    </main>
   )
 }
